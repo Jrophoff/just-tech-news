67def0048c15132c9cb494b6904234c4bd55d92f
Jordan Ophoff <noreply>
2021-12-20 23:10:47 -0500

    password security added

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our user model
-class User extends Model {}
+class User extends Model { }
 
 // define table columns and configuration
 User.init(
@@ -35,9 +36,19 @@ User.init(
             validate: {
                 len: [4]
             }
-          }  
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10)
+                return updatedUserData;
+            },
         },
-        {
         sequelize,
         timestamps: false,
         freezeTableName: true,
